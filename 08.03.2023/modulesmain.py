1e336369837220e88719bac4c2288873c52a9c7a
Gowthamraj Raja <gowthamraj.raja@aspiresys.com>
2024-01-16 17:30:33 +0530

    Python

---
+++ 08.03.2023/modulesmain.py
@@ -0,0 +1,15 @@
+import module as m
+import math
+# import module
+# from module import fun
+# from module import*
+output=dir(math)
+print(output)
+m.transport=m.vehicle()
+m.transport.bike(2,"air")
+m.transport.car(4,"oil")
+
+print("..............................")
+m.fun(5)
+print("..............................")
+m.count_case("The quick Brow Fox")
